2c9aaeee5d9c5e982716f38d9554bd3c4420a919
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-09-30 11:32:17 -0500

    renders working, added conditional

--- assets/js/pseudocode.js
+++ assets/js/pseudocode.js
@@ -1,20 +1,17 @@
-//Fetch weather data from Open Weather API
-    //construct API URL using the user's input and api key
-    //fetch data from the api using that constructed url
-    //parse the json response
-    //handle errors (like if the city is not found)
-//Display 5-day forecast
-    //extract forecast data from api response
-    //loop through forecast data, and for each of 5 days, create html elements to display info
-    //append elements to container
 //Initial page load
     //load the search history from local storage and display it using perhaps a display search history function
 
-// Forecast data paths:
-    // data.list[i].weather.main
-    // data.list[i].weather.icon ??
-    // data.list[i].wind[0].value ?
-    // data.list[i].main.humidity
-    // data.list[i].main.temp
-
-
+    var searchFormHandler = function(event) {
+        event.preventDefault();
+        var city = searchInputEl.value.trim();
+        if (city) {
+            fetchCoordinates(city);
+            renderHistory(city);
+            searchInputEl.value = '';
+            searchHistory.push(city);
+            localStorage.setItem('city', JSON.stringify(searchHistory));
+            } else {
+                alert('City not found');
+            }
+        }
+    
